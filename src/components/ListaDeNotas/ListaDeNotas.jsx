b8d2013465bc7582e7c689ee429a5fd24a9b20c4
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-20 17:30:56 -0300

    Fim do primeiro módulo de React

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -7,10 +7,10 @@ export default class ListaDeNotas extends Component {
 	render() {
 		return (
 			<ul className='lista-notas'>
-                {Array.of('Trabalho', 'Trabalho', 'Estudos').map((categoria, index) => {
+                {this.props.notas.map((nota, index) => {
                     return(
                     <li key={index} className='lista-notas_item'>
-                        <CardNota/>
+                        <CardNota titulo={nota.titulo} texto={nota.texto}/>
                     </li>
                     );
                 })}
