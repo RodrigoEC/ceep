c67c9b7e460a5ed1217b3af94ae0869b82c29e56
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-20 16:06:29 -0300

    Fim da aula 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import ListaDeNotas  from "./components/listaDeNotas"
-import FormularioCadastro from "./components/formularioCadastro"
+import ListaDeNotas  from "./components/ListaDeNotas"
+import FormularioCadastro from "./components/FormularioCadastro"
+import './assets/App.css';
+import './assets/index.css';
 
 export default class App extends Component {
   render() {
     return (
-      <section>
+      <section className='conteudo'>
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
